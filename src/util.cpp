750668f02707bd9bb91578328463e61f0f2eef20
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-13 14:40:51 +0900

    feat: implimented utility

---
+++ src/util.cpp
@@ -0,0 +1,29 @@
+#include "util.hpp"
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string get_date_time_str() {
+  time_t t = time(nullptr);
+  const tm *localTime = localtime(&t);
+  stringstream s;
+  s << localTime->tm_year + 1900;
+  // zero fill
+  s << setw(2) << setfill('0') << localTime->tm_mon + 1;
+  s << setw(2) << setfill('0') << localTime->tm_mday;
+  s << setw(2) << setfill('0') << localTime->tm_hour;
+  s << setw(2) << setfill('0') << localTime->tm_min;
+  s << setw(2) << setfill('0') << localTime->tm_sec;
+
+  return s.str();
+}
+
+BOOL INS_InMain(INS ins) {
+  ADDRINT inst_addr = INS_Address(ins);
+  IMG img = IMG_FindByAddress(inst_addr);
+  return IMG_Valid(img) && IMG_IsMainExecutable(img);
+}
---
+++ src/util.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+#include "pin.H"
+
+std::string get_date_time_str();
+BOOL INS_InMain(INS ins);
